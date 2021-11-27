f8953d707263edc4a302670d22cb2cdca504041c
maximepasquier <kilian.pasquier@hotmail.com>
2021-11-27 13:57:11 +0100

    mouvements

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,38 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <random>
+
+enum Rotation
+{
+	up,
+	down,
+	left,
+	right
+};
+
+enum Snake_parts
+{
+	head,
+	body_straight,
+	body_turn,
+	tail,
+	empty
+};
+
+typedef struct snake
+{
+	Snake_parts p;
+	Rotation r;
+} snake;
 
 //*Screen dimension constants
+const int ASSETS_SIZE = 40;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int GRID_LINES = SCREEN_WIDTH / ASSETS_SIZE;
+const int GRID_COLUMNS = SCREEN_HEIGHT / ASSETS_SIZE;
 
 //*Starts up SDL and creates window
 bool init();
@@ -30,8 +58,14 @@ SDL_Surface *gsnakehead = NULL;
 SDL_Surface *gsnaketail = NULL;
 SDL_Surface *gfruit = NULL;
 
-
 SDL_Surface *loadSurface(std::string path);
+
+//* Grid
+snake **grid;
+
+//* Generator
+unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+std::default_random_engine generator(seed);
 
 bool init()
 {
@@ -132,6 +166,33 @@ void close()
 
 int main(int argc, char *args[])
 {
+	//* Init grid game
+	grid = new snake *[GRID_LINES];
+	for (int i = 0; i < GRID_LINES; i++)
+	{
+		grid[i] = new snake[GRID_COLUMNS];
+	}
+
+	//* Set grid game to empty
+	for (int i = 0; i < GRID_LINES; i++)
+	{
+		for (int j = 0; j < GRID_COLUMNS; j++)
+		{
+			grid[i][j].p = empty;
+		}
+	}
+
+	//* Init game
+	//+ Start game with the snake at a fixes position
+	int snake_head_x = GRID_LINES / 2;
+	int snake_head_y = GRID_COLUMNS / 2;
+	//+ Define the head of the snake, rotation to the top
+	grid[snake_head_x][snake_head_y].p = head;
+	grid[snake_head_x][snake_head_y].r = up;
+	//+ Define the tail of the snake, rotation to the bottom
+	grid[snake_head_x][snake_head_y + 1].p = tail;
+	grid[snake_head_x][snake_head_y + 1].r = down;
+
 	//*Start up SDL and create window
 	if (!init())
 	{
@@ -162,18 +223,30 @@ int main(int argc, char *args[])
 						{
 						case SDLK_UP:
 							std::cout << "UP" << std::endl;
+							grid[snake_head_x][snake_head_y].p = body_straight;
+							snake_head_y--;
+							grid[snake_head_x][snake_head_y].p = head;
 							break;
 
 						case SDLK_DOWN:
 							std::cout << "DOWN" << std::endl;
+							grid[snake_head_x][snake_head_y].p = body_straight;
+							snake_head_y++;
+							grid[snake_head_x][snake_head_y].p = head;
 							break;
 
 						case SDLK_LEFT:
 							std::cout << "LEFT" << std::endl;
+							grid[snake_head_x][snake_head_y].p = body_straight;
+							snake_head_x--;
+							grid[snake_head_x][snake_head_y].p = head;
 							break;
 
 						case SDLK_RIGHT:
 							std::cout << "RIGHT" << std::endl;
+							grid[snake_head_x][snake_head_y].p = body_straight;
+							snake_head_x++;
+							grid[snake_head_x][snake_head_y].p = head;
 							break;
 
 						default:
@@ -183,14 +256,46 @@ int main(int argc, char *args[])
 				}
 				//*Draw the background
 				SDL_BlitSurface(gbackground, NULL, gScreenSurface, NULL);
+				/*
 				SDL_BlitSurface(gsnakehead, NULL, gScreenSurface, NULL);
 				SDL_Rect gomec;
 				gomec.x = 0;
 				gomec.y = 40;
 				SDL_BlitSurface(gsnakebodystraight, NULL, gScreenSurface, &gomec);
-
+				*/
+				//* Draw snake
+				for (int line = 0; line < GRID_LINES; line++)
+				{
+					for (int column = 0; column < GRID_COLUMNS; column++)
+					{
+						if (grid[line][column].p != empty)
+						{
+							SDL_Rect image_position;
+							image_position.x = line * ASSETS_SIZE;
+							image_position.y = column * ASSETS_SIZE;
+							switch (grid[line][column].p)
+							{
+							case head:
+								SDL_BlitSurface(gsnakehead, NULL, gScreenSurface, &image_position);
+								break;
+							case body_straight:
+								SDL_BlitSurface(gsnakebodystraight, NULL, gScreenSurface, &image_position);
+								break;
+							case body_turn:
+								SDL_BlitSurface(gsnakebodytrun, NULL, gScreenSurface, &image_position);
+								break;
+							case tail:
+								SDL_BlitSurface(gsnaketail, NULL, gScreenSurface, &image_position);
+								break;
+							default:
+								break;
+							}
+						}
+					}
+				}
 				//*Update the surface
 				SDL_UpdateWindowSurface(gWindow);
+				SDL_Delay(500);
 			}
 		}
 	}
